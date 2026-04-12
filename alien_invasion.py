3aa58e700e04e910d6156daf8da7c3101260b98a
Darian Bruce <darianmariebruce@gmail.com>
2026-04-12 08:51:09 -0400

    first commit

---
+++ alien_invasion.py
@@ -0,0 +1,16 @@
+'''
+Final Project: Alien Invasion (Milestone 1)
+Darian Marie Bruce
+04/12/2026
+The purpose of this milestone is to modify the base
+game to change the ship's orientation and movement'''
+
+import sys
+import pygame
+
+class AlienInvasion:
+    def __init__ (self) -> None: 
+        pygame.init()
+
+if __name__ == '__main__':
+    pass
