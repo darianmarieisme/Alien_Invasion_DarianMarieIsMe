71269a95bf2baab0ad65b9310da6c0621870d514
Darian Bruce <darianmariebruce@gmail.com>
2026-04-12 19:29:31 -0400

    positioned starting position on left side of screen, rotated ship

--- ship.py
+++ ship.py
@@ -21,16 +21,17 @@ class Ship:
         self.boundaries = self.screen.get_rect()
 
         self.image = pygame.image.load(self.settings.ship_file)
+        self.image = pygame.transform.rotate(self.image, -90)
         self.image = pygame.transform.scale(self.image, 
             (self.settings.ship_w, self.settings.ship_h)
             )
         
 
         self.rect = self.image.get_rect()
-        self.rect.midbottom = self.boundaries.midbottom
+        self.rect.midleft = self.boundaries.midleft
         self.moving_right = False
         self.moving_left = False
-        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
         self.arsenal = arsenal
 
     def update(self):
@@ -39,11 +40,11 @@ class Ship:
 
         temp_speed = self.settings.ship_speed
         if self.moving_right and self.rect.right < self.boundaries.right:
-            self.x += temp_speed
+            self.y += temp_speed
         if self.moving_left and self.rect.left > self.boundaries.left:
-            self.x -= temp_speed
+            self.y -= temp_speed
 
-        self.rect.x = self.x
+        self.rect.y = self.y
 
 
     def draw(self) -> None:
